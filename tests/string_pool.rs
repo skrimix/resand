635b4e59e2af2149a33daa56783f87d81f18c445
fieryhenry <henry+dev@fyhenry.uk>
2025-05-02 18:53:13 +0100

    changed license to apache 2.0
    changed from gplv3 to apache 2.0

--- tests/string_pool.rs
+++ tests/string_pool.rs
@@ -1,24 +1,7 @@
-/*
-    Copyright (C) 2024 fieryhenry
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
-
 use std::io::Cursor;
 
 use binrw::{BinReaderExt, BinWriterExt};
-use libaxml::string_pool::{
+use resand::string_pool::{
     calc_length16, calc_length8, new_length16, new_length8, ResStringPoolRef, ResStringPoolSpan,
     StringPoolFlags, StringPoolString16, StringPoolString8,
 };
